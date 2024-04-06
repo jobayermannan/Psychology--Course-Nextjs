144e7811b174841c7c33212cc20d6ef936124275
jobayermannan <jobayermannan777@gmail.com>
2024-04-06 17:07:49 +0600

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar_menu";
+import { cn } from "@/utils/cn";
+
+import Link from "next/link";
+
+
+
+const Navbar = ({ className }: { className?: string }) => {
+
+	const [active, setActive] = useState<string | null>(null);
+  return (
+	 <div
+	 className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+	 > 
+		 <Menu  setActive={setActive}>
+            <Link href={"#"}>
+				      <MenuItem item="Home" setActive={setActive} active={active} >
+						    test
+						</MenuItem>
+				</Link>
+
+
+				<MenuItem item="Our Courses" setActive={setActive} active={active} >
+						  <div  className="flex flex-col space-y-4 text-sm">
+						  <HoveredLink href={"/courses"}>	Introduction to Psychology</HoveredLink>
+						  <HoveredLink href={"/courses"}>	Cognitive Psychology</HoveredLink>
+						  <HoveredLink href={"/courses"}>	Developmental Psychology</HoveredLink>
+						  <HoveredLink href={"/courses"}>	Social Psychology</HoveredLink>
+						  <HoveredLink href={"/courses"}>	abnormal-psychology</HoveredLink>
+						  </div>
+				</MenuItem>
+
+				<Link href={"/contacts"}>
+
+					<MenuItem  item="Contact Us" setActive={setActive} active={active} >
+
+
+					</MenuItem>
+				 
+				</Link>
+		 </Menu>
+	 </div>
+  )
+}
+
+export default Navbar
