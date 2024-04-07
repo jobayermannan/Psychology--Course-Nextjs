712ae4ce0d630c6d430fc98116a6264a41853cea
jobayermannan <jobayermannan777@gmail.com>
2024-04-07 16:53:38 +0600

    upcoming webnars

--- src/components/UpComingWebiners.tsx
+++ src/components/UpComingWebiners.tsx
@@ -5,7 +5,12 @@ const UpComingWebinars = () => {
   return (
 	 <div className="p-12 bg-gray-900">
 	  <div className= "max-w-7xl mx-auto px-4 sm:px-6">
-         
+         <div className="text-center">1
+			    <h2>Featured Webinars</h2>
+				 
+			</div>
+			<div className="mt-10 text-center">2 </div>
+			<div>3</div>
 	  </div>
 
 	 </div>
