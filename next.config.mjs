a3b33f08efdc472ae851941d4632f1905ec828f4
jobayermannan <jobayermannan777@gmail.com>
2024-04-10 15:02:12 +0600

    everything setup done

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images:{
-        domains:['images.unsplash.com','ibb.co']
-    }
-};
+// next.config.js
+export default {
+    images: {
+      remotePatterns: [
+        { protocol: 'https', hostname: 'res.cloudinary.com' },
+        { protocol: 'https', hostname: 'images.unsplash.com' },
+        { protocol: 'https', hostname: 'i.ibb.co' },
+        { protocol: 'https', hostname: 'imgbb.com' },
+        { protocol: 'https', hostname: 'img.freepik.com' },
+      ],
+    },
+  }
 
-export default nextConfig;
+
+
+
+
